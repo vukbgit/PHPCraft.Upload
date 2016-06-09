3ba0bc80f383fbfc081ce030e11abac42cd635a4
vukbgit <info@vuk.bg.it>
2016-06-09 15:38:52 +0200

    first real implementation

--- src/UploadInterface.php
+++ src/UploadInterface.php
@@ -10,9 +10,53 @@ namespace PHPCraft\Upload;
 interface UploadInterface
 {
     /**
-     * Sets container
+     * Sets field to be processed
      *
-     * @param string path
+     * @param string $field
      **/
-    public function setContainer($path);
+    public function setField($field);
+    
+    /**
+     * Gets field to be processed
+     **/
+    public function getField();
+    
+    /**
+     * Sets destination
+     *
+     * @param string $path
+     **/
+    public function setDestination($path);
+    
+    /**
+     * Adds a validation rule
+     *
+     * @param string $type as defined into adapted class
+     * @param array $options rule options, if any
+     * @param array $message localized message for rule validation failure
+     **/
+    public function addValidationRule($type, $options = null, $message = null);
+    
+    /**
+     * Processes uploaded file
+     * @return boolean upload outcome
+     **/
+    public function process();
+    
+    /**
+     * Gets any generated message
+     * @return array of messages
+     **/
+    public function getMessages();
+    
+    /**
+     * Gets any information about uploaded file
+     * @return array
+     **/
+    public function getUploadedFileInfo();
+    
+    /**
+     * Performs any action to close upload operation
+     **/
+    public function close();
 }
--- src/UploadSiriusphpUploadAdapter.php
+++ src/UploadSiriusphpUploadAdapter.php
@@ -2,28 +2,103 @@
 
 namespace PHPCraft\Upload;
 
-use Sirius\Upload\Handle;
+use Sirius\Upload\Handler;
 
 /**
- * Manages uploads using siriusphp/upload  (https://github.com/siriusphp/upload)
+ * Manages uploads using codeguy/upload  (https://github.com/codeguy/Upload.git)
  *
  * @author vuk <info@vuk.bg.it>
  */
 class UploadSiriusPHPUploadAdapter implements UploadInterface
 {
+    protected $upload;
+    protected $field;
+    protected $result;
+    
     /**
-     * Constructor.
+     * Sets field to be processed
      *
+     * @param string $field
      **/
-    public function __construct()
+    public function setField($field)
     {
+        $this->field = $field;
     }
-
+    
     /**
-     * Sets container
-     *
-     * @param string path
+     * Gets field to be processed
      **/
-    public function setContainer($path){
+    public function getField()
+    {
+        return $this->field;
+    }
+    
+    /**
+     * Sets destination
+     *
+     * @param string $path
+     **/
+    public function setDestination($path)
+    {
+        $this->upload = new \Sirius\Upload\Handler($path);
+    }
+    
+    /**
+     * Adds a validation rule
+     *
+     * @param string $type as defined into adapted class
+     * @param array $options rule options, if any
+     * @param array $message localized message for rule validation failure
+     **/
+    public function addValidationRule($type, $options = null, $message = null)
+    {
+        $this->upload->addRule($type, $options, $message);
+    }
+    
+    /**
+     * Processes uploaded file
+     * @return boolean upload outcome
+     **/
+    public function process()
+    {
+        $this->result = $this->upload->process($_FILES[$this->field]);
+        return $this->result->isValid();
+    }
+    
+    /**
+     * Gets any generated message
+     * @return array of messages
+     **/
+    public function getMessages()
+    {
+        if(!isset($this->result)) {
+            return;
+        }
+        $messages = array();
+        foreach((array) $this->result->getMessages() as $message) {
+            $messages[] = $message->__tostring();
+        }
+        return $messages;
+    }
+    
+    /**
+     * Gets any information about uploaded file
+     * @return array
+     **/
+    public function getUploadedFileInfo()
+    {
+        return [
+            'name' => $this->result->name
+        ];
+    }
+    
+    /**
+     * Performs any action to close upload operation
+     **/
+    public function close()
+    {
+        if($this->result->isValid()) {
+            $this->result->confirm();
+        }
     }
 }
